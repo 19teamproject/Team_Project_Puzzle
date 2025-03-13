e45f8a3f44d8dcda8a03bf3c55bf2629f6d6012d
cookie35 <hakyeong35@gmail.com>
2025-03-13 16:36:19 +0900

    Feat: 물 효과 추가 중

--- Assets/Scripts/Elemental/FireElement/Fire.cs
+++ Assets/Scripts/Elemental/FireElement/Fire.cs
@@ -6,23 +6,32 @@ using UnityEngine;
 public class Fire : MonoBehaviour
 {
     public GameObject fireEffect;  // 불이 붙을 이펙트
+    public bool isFireActive = true;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Flammable")) // 가연성 오브젝트(ex.woodBlock)
         {
-
             Destroy(other.gameObject);
         }
         else if(other.CompareTag("Ice"))  // 얼음을 녹임
         {
             Destroy(other.gameObject);
         }
-        else if(other.CompareTag("Torch"))  // 횃불에 불 붙이기
+        else if(other.CompareTag("Water"))
         {
-            other.GetComponent<Torch>().Ignite();
+            ExtinguishFire();
         }
 
+    }
+
+    void ExtinguishFire()
+    {
+        if(isFireActive)
+        {
+            isFireActive = false;
+            fireEffect.SetActive(false);
+        }
     }
 
 }
--- Assets/Scripts/Elemental/WaterElement/Water.cs
+++ Assets/Scripts/Elemental/WaterElement/Water.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class Water : MonoBehaviour
-//{
-//    private void OnTriggerEnter(Collider other)
-//    {
-//        if(other.CompareTag("Fire"))
-//        {
-//            Destroy(other.gameObject);  // 불을 끔    
-//        }
-//        else if(other.CompareTag("WaterDevice")) //  장치를 돌림
-//        {
-//            other.GetComponent<WaterDevice>().Spin();
-//        }
-//    }
-//}
+public class Water : MonoBehaviour
+{
+    public GameObject waterEffectPrefab; // 물줄기 이펙트
+    public Transform fireObject; // 불 오브젝트
+    public float waterForce = 10f; // 물줄기 발사 강도
+
+    private void Update()
+    {
+       if (Input.GetMouseButtonDown(0))  // 마우스 클릭하면 물총 발사
+       {
+            FireWater(); 
+       }
+    }
+
+    void FireWater()
+    {
+        GameObject waterEffect = Instantiate(waterEffectPrefab, transform.position, Quaternion.identity);
+        Rigidbody rigid = waterEffect.GetComponent<Rigidbody>();
+        Vector2 fireDirection = transform.up; // 물총이 향하는 방향
+
+        rigid.velocity = fireDirection * waterForce; // 물줄기 발사
+
+        Destroy(waterEffect, 2f);
+    }
+}
