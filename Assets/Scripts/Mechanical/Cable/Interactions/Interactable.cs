145f6f6c50a78a18677e766691cd44069a430dc6
Michael <kimjinhong01@naver.com>
2025-03-13 16:35:41 +0900

    <Feat> 주석 깨짐 방지 설정

--- Assets/Scripts/Mechanical/Cable/Interactions/Interactable.cs
+++ Assets/Scripts/Mechanical/Cable/Interactions/Interactable.cs
@@ -6,8 +6,10 @@ namespace HInteractions
     [DisallowMultipleComponent]
     public class Interactable : MonoBehaviour
     {
+        // 상호작용 가능한 오브젝트를 보고있으면 에임 표시
         [field: SerializeField] public bool ShowPointerOnInterract { get; private set; } = true;
 
+        // 
         [field: SerializeField, ReadOnly] public bool IsSelected { get; private set; }
 
         protected virtual void Awake()
