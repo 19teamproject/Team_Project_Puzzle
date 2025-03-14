e3fe4e30649cd248a6cc542ed97ebaf29146c10b
25maru <qkrwldnjs6694@gmail.com>
2025-03-14 17:52:53 +0900

    Refactor: 큐브 스크립트 정리

--- Assets/Scripts/Environment/Cube/CubeBoneScaler.cs
+++ Assets/Scripts/Environment/Cube/CubeBoneScaler.cs
@@ -1,22 +1,49 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-
-public enum ScaleDir
-{
-    X, Y, Z
-}
+using NaughtyAttributes;
 
 public class CubeBoneScaler : MonoBehaviour
 {
     [SerializeField] private List<Transform> xBones;
     [SerializeField] private List<Transform> yBones;
     [SerializeField] private List<Transform> zBones;
+    [SerializeField] private float maxScale = 3f;
     [SerializeField] private float offset = 0.08f;
     [SerializeField] private float speed = 1f;
 
+    [Space(10f)]
+    [SerializeField] private BoxCollider boxCollider;
+
     private Vector3 currentScale = Vector3.one;
+
+    [Foldout("Debug")]
+    [SerializeField] private float xScale = 1f;
+    [Foldout("Debug")]
+    [SerializeField] private float yScale = 1f;
+    [Foldout("Debug")]
+    [SerializeField] private float zScale = 1f;
+
+    [Button]
+    private void DebugUpdateScale()
+    {
+        DebugUpdateScale(xScale, yScale, zScale);
+    }
+
+    private void DebugUpdateScale(float x, float y, float z)
+    {
+        foreach (Transform t in xBones)
+            t.localPosition = new(x - offset, t.localPosition.y, t.localPosition.z);
+
+        foreach (Transform t in yBones)
+            t.localPosition = new(t.localPosition.x, y - offset, t.localPosition.z);
+
+        foreach (Transform t in zBones)
+            t.localPosition = new(t.localPosition.x, t.localPosition.y, z - offset);
+
+        boxCollider.center = new Vector3(x, y, z) / 2f;
+        boxCollider.size = new Vector3(x, y, z);
+    }
 
     private void Update()
     {
@@ -41,28 +68,37 @@ public class CubeBoneScaler : MonoBehaviour
         {
             case ScaleDir.X:
                 currentScale.x += amount;
-                currentScale.x = Mathf.Max(currentScale.x, 0f);
+                currentScale.x = Mathf.Clamp(currentScale.x, 1f, maxScale);
                 
                 foreach (Transform t in xBones)
                     t.DOMoveX(currentScale.x - offset, speed);
+
+                DOTween.To(() => boxCollider.center, x => boxCollider.center = x, new Vector3(currentScale.x / 2f, boxCollider.center.y, boxCollider.center.z), speed);
+                DOTween.To(() => boxCollider.size, x => boxCollider.center = x, new Vector3(currentScale.x, boxCollider.center.y, boxCollider.center.z), speed);
 
                 break;
 
             case ScaleDir.Y:
                 currentScale.y += amount;
-                currentScale.y = Mathf.Max(currentScale.y, 0f);
+                currentScale.y = Mathf.Clamp(currentScale.y, 1f, maxScale);
 
                 foreach (Transform t in yBones)
                     t.DOMoveY(currentScale.y - offset, speed);
+
+                DOTween.To(() => boxCollider.center, x => boxCollider.center = x, new Vector3(boxCollider.center.x, currentScale.y / 2f, boxCollider.center.z), speed);
+                DOTween.To(() => boxCollider.size, x => boxCollider.center = x, new Vector3(boxCollider.center.x, currentScale.y, boxCollider.center.z), speed);
 
                 break;
 
             case ScaleDir.Z:
                 currentScale.z += amount;
-                currentScale.z = Mathf.Max(currentScale.z, 0f);
+                currentScale.z = Mathf.Clamp(currentScale.z, 1f, maxScale);
 
                 foreach (Transform t in zBones)
                     t.DOMoveZ(currentScale.z - offset, speed);
+
+                DOTween.To(() => boxCollider.center, x => boxCollider.center = x, new Vector3(boxCollider.center.x, boxCollider.center.y, currentScale.y / 2f), speed);
+                DOTween.To(() => boxCollider.size, x => boxCollider.center = x, new Vector3(boxCollider.center.x, boxCollider.center.y, currentScale.y), speed);
 
                 break;
         }
