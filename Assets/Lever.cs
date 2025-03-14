d6d296afd3220f7d13f2f490c8e5be8dba7ab1fc
윤동영 <ehddud9608@naver.com>
2025-03-15 01:59:51 +0900

    Feat: 레버 애니메이션 추가
    레버 애니메이션 추가

--- Assets/Lever.cs
+++ Assets/Lever.cs
@@ -1,18 +1,38 @@
+using cakeslice;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lever : MonoBehaviour
+public class Lever : MonoBehaviour, IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Data")]
+    [SerializeField] protected CubeData data;
+    [SerializeField] protected Outline outline;
+    [SerializeField] 
+
+    private Cube cube;
+
+    private bool isMove = false;
+
+    public string GetInteractPrompt()
     {
-        
+        string str = $"<font=\"GmarketSansMedium SDF\" material=\"GmarketSansMedium SDF Glow Blue\">{data.displayName}</font> - {data.description}";
+        return str;
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool OnInteract()
     {
-        
+        if(cube != null)
+        {
+            cube.OnInteract(); //여기에 상호작용시 하고싶은 메서드 입력
+            return true;
+        }
+        Debug.Log("Cube가 연결되지 않았습니다.");
+        return false;
+    }
+
+    public virtual void SetOutline(bool show)
+    {
+        if (outline != null) outline.color = show ? 0 : 1;
     }
 }
