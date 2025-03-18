c9e0f2724e89f2f179c44991ab13f06d3dd806ef
Michael <kimjinhong01@naver.com>
2025-03-18 17:28:49 +0900

    Fix: 버그수정

--- Assets/Scripts/Player/Interaction.cs
+++ Assets/Scripts/Player/Interaction.cs
@@ -205,7 +205,7 @@ public class Interaction : MonoBehaviour, IObjectHolder
         Vector3 handRot;
         if (HeldObject.tag == "ElectricBox")
         {
-            HeldObject.rb.velocity = (handTransform.position - HeldObject.transform.position) * holdingForce;
+            HeldObject.transform.position = handTransform2.position;
             handRot = handTransform2.rotation.eulerAngles;
         }
         else
